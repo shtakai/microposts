62aa55f6e03ea71a1e41ed6cfbe61b1183e0ca34
Alyson T <shtakai@gmail.com>
2016-11-12 12:08:52 +0000

    migrated favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109063910) do
+ActiveRecord::Schema.define(version: 20161112120301) do
+
+  create_table "favolites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "micropost_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "favolites", ["micropost_id"], name: "index_favolites_on_micropost_id"
+  add_index "favolites", ["user_id", "micropost_id"], name: "index_favolites_on_user_id_and_micropost_id", unique: true
+  add_index "favolites", ["user_id"], name: "index_favolites_on_user_id"
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
